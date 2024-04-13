19d34d783537719bea02a20e1d44129db72484a9
Anushka2003-raghuvanshi <141910696+Anushka2003-raghuvanshi@users.noreply.github.com>
2024-04-13 23:48:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+document.getElementById("circular-button").addEventListener("click", function() {
+  // Navigate to "Android Large - 2"
+  window.location.href = "dacoit2.html";
+
+  // Add animation classes to trigger Smart animate with specified timing function and duration
+  this.classList.add("smart-animate");
+});
+
+document.getElementById("text1").addEventListener("click", function() {
+  // Navigate to "Android Large - 4"
+  window.location.href = "androidlarge4.html";
+
+  // Add animation classes to trigger Smart animate with Spring effect
+  // You can use JavaScript to add CSS classes dynamically
+  document.getElementById("text").classList.add("smart-animate", "spring-animation");
+});
+
+document.getElementById("buttonm").addEventListener("click", function() {
+  // Navigate to "Android Large - 4"
+  window.location.href = "large5.html";
+
+  // Add animation classes to trigger Smart animate with Spring effect
+  // You can use JavaScript to add CSS classes dynamically
+  document.getElementById("text").classList.add("smart-animate", "spring-animation");
+});
+
